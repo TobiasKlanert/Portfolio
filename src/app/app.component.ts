f5dae5db3fbaf10111361ae2839f4062721efb34
TobiasKlanert <tobias.klanert@t-online.de>
2025-04-06 21:51:01 +0200

    refactor atf and working on skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet, HeroComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit { 
   @ViewChildren('section') sections!: QueryList<ElementRef>;
 
   observer: IntersectionObserver;
