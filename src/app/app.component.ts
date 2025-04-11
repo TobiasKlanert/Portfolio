17cfc2459f17a1571737870908eae00d6fade93b
TobiasKlanert <tobias.klanert@t-online.de>
2025-04-11 16:43:03 +0200

    refactoring and working on projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroComponent, AboutMeComponent, SkillsComponent],
+  imports: [CommonModule, RouterOutlet, HeroComponent, AboutMeComponent, SkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
