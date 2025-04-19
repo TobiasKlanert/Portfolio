2a6e371a772954ce2cd05f9210ba3c7864d5c80c
TobiasKlanert <tobias.klanert@t-online.de>
2025-04-19 23:18:35 +0200

    added scroll snapping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  Renderer2,
-  ViewChildren,
-  QueryList,
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
@@ -33,31 +26,4 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChildren('section') sections!: QueryList<ElementRef>;
-
-  observer: IntersectionObserver;
-
-  constructor(private renderer: Renderer2) {
-    this.observer = new IntersectionObserver(this.onIntersection.bind(this), {
-      threshold: 0.6, // 60% sichtbar
-    });
-  }
-
-  ngAfterViewInit() {
-    this.sections.forEach((section) =>
-      this.observer.observe(section.nativeElement)
-    );
-  }
-
-  onIntersection(entries: IntersectionObserverEntry[]) {
-    for (const entry of entries) {
-      if (entry.isIntersecting) {
-        const bgColor = entry.target.getAttribute('data-bg');
-        if (bgColor) {
-          this.renderer.setStyle(document.body, 'background-color', bgColor);
-        }
-      }
-    }
-  }
-}
+export class AppComponent {}
