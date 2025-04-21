06a17630be56af8595b653e5b02b876d73f7bea7
TobiasKlanert <tobias.klanert@t-online.de>
2025-04-21 18:18:01 +0200

    working on menu

--- src/app/menu-overlay/menu-overlay.component.ts
+++ src/app/menu-overlay/menu-overlay.component.ts
@@ -5,10 +5,26 @@ import { Component, EventEmitter, Output } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './menu-overlay.component.html',
-  styleUrl: './menu-overlay.component.scss'
+  styleUrl: './menu-overlay.component.scss',
 })
 export class MenuOverlayComponent {
+  hoverStates: { [key: string]: boolean } = {
+    aboutMe: false,
+    skills: false,
+    portfolio: false,
+    references: false,
+    contact: false,
+  };
+
   @Output() close = new EventEmitter<void>();
+
+  onMouseEnter(section: string) {
+    this.hoverStates[section] = true;
+  }
+
+  onMouseLeave(section: string) {
+    this.hoverStates[section] = false;
+  }
 
   closeMenu() {
     this.close.emit();
