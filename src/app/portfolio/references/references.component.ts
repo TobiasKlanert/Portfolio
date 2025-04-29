2309693012a9f0457b53a943550ec71cf09ef9c9
TobiasKlanert <tobias.klanert@t-online.de>
2025-04-29 22:23:33 +0200

    translated references

--- src/app/portfolio/references/references.component.ts
+++ src/app/portfolio/references/references.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslationService } from '../../services/translation.service';
 
 @Component({
   selector: 'app-references',
@@ -9,23 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
+
+  constructor(public translationService: TranslationService) {}
+
   references = [
     {
       name: 'Max Mustermann',
-      feedback: 'Die Zusammenarbeit mit dir war stets zuverlässig, kreativ und lösungsorientiert.',
-      role: 'Teamleiter Frontend bei Musterfirma GmbH',
       link: 'https://de.linkedin.com/'
     },
     {
       name: 'Anna Beispiel',
-      feedback: 'Besonders beeindruckt hat mich dein technisches Verständnis und deine Eigeninitiative.',
-      role: 'Projektmanagerin bei Beispiel AG',
       link: 'https://de.linkedin.com/'
     },
     {
       name: 'Lukas Schmidt',
-      feedback: 'Deine strukturierte Arbeitsweise und dein Designgefühl haben das Projekt enorm bereichert.',
-      role: 'UX Designer bei DesignCo',
       link: 'https://de.linkedin.com/'
     }
   ];
