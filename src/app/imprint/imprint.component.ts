1e5adff0600790ec6e664fa5c246877fb80d79ee
TobiasKlanert <tobias.klanert@t-online.de>
2025-05-02 23:13:00 +0200

    finished translation of imprint

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { TranslationService } from '../services/translation.service';
 
 @Component({
   selector: 'app-imprint',
@@ -11,6 +12,9 @@ import { FooterComponent } from '../shared/components/footer/footer.component';
   styleUrl: './imprint.component.scss',
 })
 export class ImprintComponent {
+
+  constructor(public translationService: TranslationService) {}
+
   isGerman = false;
 
   switchLanguage() {
