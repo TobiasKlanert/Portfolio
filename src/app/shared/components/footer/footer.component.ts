518d40cc835bd9c87be7ffaf57e3c2eed54e352c
TobiasKlanert <tobias.klanert@t-online.de>
2025-05-04 15:53:43 +0200

    added footer to imprint and privacy policy

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslationService } from '../../../services/translation.service';
 
@@ -7,8 +7,43 @@ import { TranslationService } from '../../../services/translation.service';
   standalone: true,
   imports: [RouterModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit, OnDestroy {
   constructor(public translationService: TranslationService) {}
+
+  private sectionObserver!: IntersectionObserver;
+
+  isLightText = true;
+
+  ngAfterViewInit(): void {
+    const sections = ['contact', 'imprint', 'privacy-policy']
+      .map((id) => document.getElementById(id))
+      .filter((el): el is HTMLElement => !!el);
+
+    this.sectionObserver = new IntersectionObserver(
+      (entries) => {
+        for (const entry of entries) {
+          if (entry.target.id === 'contact') {
+            this.isLightText = entry.isIntersecting;
+          }
+          if (
+            entry.target.id === 'imprint' ||
+            entry.target.id === 'privacy-policy'
+          ) {
+            this.isLightText = false;
+          }
+        }
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    sections.forEach((section) => this.sectionObserver.observe(section));
+  }
+
+  ngOnDestroy(): void {
+    if (this.sectionObserver) this.sectionObserver.disconnect();
+  }
 }
