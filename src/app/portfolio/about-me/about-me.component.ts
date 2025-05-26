26ebf0e11cf68c9ec5b2ead55f9f33ac8ba9e466
TobiasKlanert <tobias.klanert@t-online.de>
2025-05-26 21:25:33 +0200

    bugfixing

--- src/app/portfolio/about-me/about-me.component.ts
+++ src/app/portfolio/about-me/about-me.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { TranslationService } from '../../services/translation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
+  showOverlay = false;
+
   constructor(public translationService: TranslationService) {}
+
+  openOverlay() {
+    this.showOverlay = true;
+    console.log(this.showOverlay);
+  }
+
+  closeOverlay() {
+    this.showOverlay = false;
+  }
 }
