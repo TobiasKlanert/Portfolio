30d9c9387eb21cb2d3aa74fef43c2624ef207ad2
TobiasKlanert <tobias.klanert@t-online.de>
2025-06-21 14:33:42 +0200

    fixed issue with links

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -8,12 +8,20 @@ import { FooterComponent } from '../shared/components/footer/footer.component';
   standalone: true,
   imports: [HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss'
+  styleUrl: './privacy-policy.component.scss',
 })
 export class PrivacyPolicyComponent {
   isGerman = false;
 
   switchLanguage() {
     this.isGerman = !this.isGerman;
+  }
+
+  scrollTo(id: string, event: Event) {
+    event.preventDefault();
+    const el = document.getElementById(id);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
   }
 }
