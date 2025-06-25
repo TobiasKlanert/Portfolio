1235b85b44bccaf65024ba5db8638c42da2b046b
TobiasKlanert <tobias.klanert@t-online.de>
2025-06-25 16:46:40 +0200

    fix: minor bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Renderer2 } from '@angular/core';
+import { filter, Subscription } from 'rxjs';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +11,31 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private routerSub!: Subscription;
 
+  constructor(private router: Router, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.routerSub = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        const navEnd = event as NavigationEnd;
+        if (
+          navEnd.urlAfterRedirects === '/imprint' ||
+          navEnd.urlAfterRedirects === '/privacy-policy'
+        ) {
+          this.renderer.addClass(document.body, 'imprint-bg');
+        } else {
+          this.renderer.removeClass(document.body, 'imprint-bg');
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.routerSub) {
+      this.routerSub.unsubscribe();
+    }
+    this.renderer.removeClass(document.body, 'imprint-bg');
+  }
 }
